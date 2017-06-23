8acfa5888561925d93409b09b60ef36ae328b513
Felix Eckhardt <eckhardt@rocket-solutions.de>
2017-06-23 16:25:34 +1000

    finished section 1 and 2 from udemy course https://www.udemy.com/react-redux

---
+++ src/components/video_list_item.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class VideoListItem extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    const video = this.props.video;
+    const onVideoSelect = this.props.onVideoSelect;
+    const imageUrl = video.snippet.thumbnails.default.url;
+    const title = video.snippet.title;
+    return (
+      <li onClick={() => onVideoSelect(video)} className="list-group-item">
+        <div className="video-list media">
+          <div className="media-left">
+            <img className="media-object" src={imageUrl} />
+          </div>
+          <div className="media-body">
+            <div className="media-heading">{title} </div>
+          </div>
+        </div>
+      </li>
+    )
+  };
+
+};
+
+export default VideoListItem;
